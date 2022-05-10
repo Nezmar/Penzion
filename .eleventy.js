297fd9c9860d6bf425954499f56086ac553fa129
Petra Klohnová <petra.klohnova@seznam.cz>
2022-05-10 22:51:16 +0200

    Zase commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,16 @@
 module.exports = function(eleventyConfig) {
-    // Výchozí výstupní složka je: _site
-    // Zkopírovat images/ do _site/images
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("icons");
-    // Zkopírovat css/ to _site/css/
-    eleventyConfig.addPassthroughCopy("css");
-   };
-   module.exports = function(eleventyConfig) {
-    // ...necháme, co už v konfiguraci je, a přidáme:
-    return {
-      // možné formáty šablon
-      templateFormats: ["njk", "html", "md"],
-      // jako šablonovací jazyk zvolíme Nunjucks`
-      markdownTemplateEngine: "njk",
-      htmlTemplateEngine: "njk",
-      dataTemplateEngine: "njk",
-    }
-  };
+  // ...necháme, co už v konfiguraci je, a přidáme:
+  // Výchozí výstupní složka je: _site
+  // Zkopírovat images/ do _site/images
+  eleventyConfig.addPassthroughCopy("images");
+  // Zkopírovat css/ to _site/css/
+  eleventyConfig.addPassthroughCopy("css");
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md"],
+    // jako šablonovací jazyk zvolíme Nunjucks`
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+};
